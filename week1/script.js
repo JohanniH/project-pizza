031dd104187cb362c55cdc9e1635011896a02406
Johan Häggström <h4ggstr0m@hotmail.com>
2023-10-06 22:04:18 +0200

    step1

---
+++ week1/script.js
@@ -0,0 +1,33 @@
+// Start here
+
+// Step 1 - Welcome and introduction
+// Your code goes here
+alert(
+    `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  )
+// Ask for the users name
+const userName = prompt("Welcome to our Javascript Pizzeria. Please enter your name:");
+
+// Check if the user entered a name
+if (userName !== null) {
+  // Greet the user
+  alert(`Hello, ${userName}! Ready to start?`);
+} else {
+  // If user canceled the prompt
+  alert("You didn't provide a name. Please refresh the page to try again.");
+}
+// Greet the user
+alert(`Hi ${userName}!`);
+  
+  // Step 2 - Food choice
+  // Your code goes here
+  
+  // Step 3 - Subtype choice
+  // Your code goes here
+  
+  
+  // Step 4 - Age
+  // Your code goes here
+  
+  // Step 5 - Order confirmation
+  // Your code goes here
